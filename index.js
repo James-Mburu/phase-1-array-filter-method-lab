55f6cf651053f5f02c3af8d0f7fdcc5b5332dd27
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-14 15:19:44 +0300

    commit tree 70f9ea561ca04a5134ae47572f3e9857c3d01b62

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+
+// function findMatching(drivers, name) { 
+//     let result = drivers.filter(drivers => drivers.trim().toLowerCase() == name.toLowerCase()); 
+//     return result; 
+//  } 
+
+function findMatching(drivers, name) {
+    // return a list of all drivers whose name is the same as the provided name
+      return drivers.filter(function(driver) {
+          return driver.toLowerCase() === name.toLowerCase();
+      }
+      );
+  }
+  
+  function fuzzyMatch(drivers, name) {
+    // return a list of all drivers whose name begins with the provided letters
+      return drivers.filter(function(driver) {
+          return driver.startsWith(name);
+      }
+      );
+  }
+  
+  function matchName(drivers_objects, name) {
+    // return a list of all driver objects whose name attribute matches the provided name
+      return drivers_objects.filter(function(driver) {
+          return driver.name === name;
+      }
+      );
+  }
